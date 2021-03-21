ae700d38255beecdb3e85bfe5130bbeb0fcfb3e1
drHush <allannelly690@gmail.com>
2021-03-21 13:33:52 +0300

    ft: add sales entity and entity tests

--- entity/sale.go
+++ entity/sale.go
@@ -1,1 +1,40 @@
 package entity
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"time"
+)
+
+type Sale struct {
+	Product ID
+	User ID
+	Total int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// NewSale create a new sale
+func NewSale(productId, userId ID, total int) (*Sale, error) {
+	sale := &Sale{
+		Product:   productId,
+		User:      userId,
+		Total:     total,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err := sale.Validate()
+	if err != nil {
+		return nil, ErrInvalidEntity
+	}
+	return sale, err
+}
+
+func (sale Sale) Validate() error  {
+	return validation.ValidateStruct(&sale,
+		validation.Field(&sale.User, validation.Required, is.UUID),
+		validation.Field(&sale.Product, validation.Required, is.UUID),
+		validation.Field(&sale.Total, validation.Required, validation.Min(1)),
+		)
+}
--- entity/sale_test.go
+++ entity/sale_test.go
@@ -1,1 +1,54 @@
-package entity
+package entity_test
+
+import (
+	"StoreManager-DDD/entity"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+
+func TestNewSale(t *testing.T) {
+	sale, err := entity.NewSale(entity.NewID(), entity.NewID(), 300)
+	assert.Nil(t, err)
+	assert.Equal(t, sale.Total, 300)
+	assert.IsType(t, sale.Product, entity.ID{})
+}
+
+func TestSale_Validate(t *testing.T) {
+	type test struct {
+		product entity.ID
+		user  entity.ID
+		total int
+		want  error
+	}
+	tests := []test{
+		{
+			user:  entity.NewID(),
+			product: entity.NewID(),
+			total: 300,
+			want:      nil,
+		},
+		{
+			user:  entity.NewID(),
+			product: entity.NewID(),
+			want:      entity.ErrInvalidEntity,
+		},
+		{
+			user:  entity.NewID(),
+			total:  0,
+			product: entity.NewID(),
+			want:      entity.ErrInvalidEntity,
+		},
+		{
+			user:  entity.NewID(),
+			product: entity.NewID(),
+			total: -1000,
+			want:  entity.ErrInvalidEntity,
+		},
+	}
+	for _, tc := range tests {
+
+		_, err := entity.NewSale(tc.product, tc.user, tc.total)
+		assert.Equal(t, err, tc.want)
+	}
+}
