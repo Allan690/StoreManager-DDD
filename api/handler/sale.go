7bebe8978853cbac532477e99902fe4855e83471
drHush <allannelly690@gmail.com>
2021-04-05 10:54:32 +0300

    feat: sales endpoint with tests for some methods

--- api/handler/sale.go
+++ api/handler/sale.go
@@ -1,1 +1,106 @@
 package handler
+
+import (
+	"StoreManager-DDD/entity"
+	"StoreManager-DDD/usecase/sale"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
+	"log"
+	"net/http"
+	"time"
+)
+
+var CreateSaleStruct struct {
+	Product uuid.UUID `json:"product"`
+	User uuid.UUID `json:"user"`
+	Total int `json:"total"`
+}
+
+
+// ListSales handles listing of sales
+func ListSales(service sale.Usecase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userList, err := service.GetAllSales()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "success", "sales": &userList})
+	}
+}
+
+// CreateSales handles creation of a sale
+// returns a handler function that processes the http request
+func CreateSales(service sale.Usecase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := c.Bind(&CreateSaleStruct)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": err.Error()})
+			return
+		}
+		sale := entity.Sale{}
+		sale.ID = entity.ID{UUID: entity.NewID()}
+		sale.Product = entity.ID{UUID: CreateSaleStruct.Product}
+		sale.User = entity.ID{UUID: CreateSaleStruct.User}
+		sale.CreatedAt = time.Now()
+		sale.UpdatedAt = time.Now()
+		newSale, err := service.CreateSale(&sale)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": err.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, gin.H{"status": "success", "sale": newSale})
+	}
+}
+
+//GetSalesByUserId gets sales by a certain user using their id
+func GetSalesByUserId(service sale.Usecase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := uuid.FromString(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": "invalid id"})
+			return
+		}
+		sales, err := service.GetSalesByUserId(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "success", "sales": sales})
+	}
+}
+
+// DeleteSale handles creation of a sale and returns a handler function that
+//handles the incoming http request.
+// the sale service is injected as a dependency
+func DeleteSale(service sale.Usecase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := uuid.FromString(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": "invalid id"})
+			return
+		}
+		_, err = service.DeleteSale(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": err.Error()})
+			return
+		}
+		c.JSON(http.StatusNoContent, gin.H{"status": "success",
+			"message": fmt.Sprintf( "sale deleted!")})
+	}
+}
+
+
+func MakeSalesHandlers(r *gin.Engine, service sale.Usecase) *gin.Engine {
+	api := r.Group("/api")
+
+	{
+		api.GET("/sale",  ListSales(service))
+		api.POST("/sale", CreateSales(service))
+		api.GET("/sale/user/:id", GetSalesByUserId(service))
+		api.DELETE("/sale/:id", DeleteSale(service))
+	}
+	return r
+}
--- api/handler/sale_test.go
+++ api/handler/sale_test.go
@@ -1,1 +1,89 @@
 package handler_test
+
+import (
+	"StoreManager-DDD/api/handler"
+	"StoreManager-DDD/entity"
+	mock "StoreManager-DDD/usecase/sale/mock"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+)
+
+var mockProduct = uuid.Must(uuid.NewV4())
+var mockUser = uuid.Must(uuid.NewV4())
+var mockSale = entity.Sale{
+	ID:        entity.ID{UUID: entity.NewID()},
+	Product:   entity.ID{UUID: mockProduct},
+	User:      entity.ID{UUID: mockUser},
+	Total:     100,
+	CreatedAt: time.Now(),
+	UpdatedAt: time.Now(),
+}
+var mockSalesList []*entity.Sale
+
+func TestCreateSales(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	m := mock.NewMockUsecase(controller)
+	r := gin.Default()
+	r = handler.MakeSalesHandlers(r, m)
+	m.EXPECT().
+		CreateSale(gomock.Any()).
+		Return(&mockSale, nil)
+	handler.CreateSales(m)
+	w := httptest.NewRecorder()
+	payload := fmt.Sprintf( `{
+		"product": %s,
+		"user": %s,
+		"total": %d
+		}`,uuid.Must(uuid.NewV4()), uuid.Must(uuid.NewV4()), 500)
+	req, err := http.NewRequest("POST", "/api/sale", strings.NewReader(payload))
+	r.ServeHTTP(w, req)
+	sec := map[string]interface{}{}
+	_ = json.Unmarshal([]byte(w.Body.String()), &sec)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, w.Code)
+	assert.Equal(t, sec["status"], "success")
+}
+
+func TestGetSalesByUserId(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	m := mock.NewMockUsecase(controller)
+	r := gin.Default()
+	r = handler.MakeSalesHandlers(r, m)
+	mockSalesList = append(mockSalesList, &mockSale, &mockSale)
+	m.EXPECT().
+		GetSalesByUserId(mockSale.User.UUID).
+		Return(mockSalesList, nil)
+	handler.GetSalesByUserId(m)
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", fmt.Sprintf("/api/sale/user/%s", mockSale.User.UUID), nil)
+	r.ServeHTTP(w, req)
+	sec := map[string]interface{}{}
+	_ = json.Unmarshal([]byte(w.Body.String()), &sec)
+	fmt.Println(sec)
+	assert.Nil(t, err)
+	assert.Len(t, sec, 2)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+//func TestListSales(t *testing.T) {
+//	gin.SetMode(gin.TestMode)
+//	controller := gomock.NewController(t)
+//	defer controller.Finish()
+//	m := mock.NewMockUsecase(controller)
+//	r := gin.Default()
+//	r = handler.MakeSalesHandlers(r, m)
+//	m.EXPECT().GetAllSales().Return(&mockSale, nil)
+//}
